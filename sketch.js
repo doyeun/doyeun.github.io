1d015d19608b76de11a5781057886db9fdeda87b
doyeun <41002361+doyeun@users.noreply.github.com>
2018-07-09 17:45:35 +0200

    ball and box v4

--- sketch.js
+++ sketch.js
@@ -6,12 +6,15 @@ function draw() {
   background('green');
 
   fill("orange");
-  rect(450,175,80,80);
+  noStroke();
+  rect(300,150,80,80);
 
   fill("yellow");
   ellipse(mouseX,mouseY,80,80);
+  fill("orange");
+  ellipse(mouseX,mouseY,20,20);
 
   fill("white");
   textSize(35);
-  text("input! -->",50,200);
+  text("input!",50,200);
 }
