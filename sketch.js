ba9459f3aeacdf8f5354592cf89bcd7b57a32ce8
doyeun <41002361+doyeun@users.noreply.github.com>
2018-07-09 16:07:18 +0200

    ball and box v3

--- sketch.js
+++ sketch.js
@@ -6,12 +6,12 @@ function draw() {
   background('green');
 
   fill("orange");
-  rect(450,200,80,80);
+  rect(450,175,80,80);
 
   fill("yellow");
   ellipse(mouseX,mouseY,80,80);
 
   fill("white");
   textSize(35);
-  text("put the yellow ball in the box! -->",50,200);
+  text("input! -->",50,200);
 }
